7bb1230affac1a7a88ab051006804b326af98c92
James Tuten <james.tuten@red-gate.com>
2014-08-12 11:23:33 +0100

    Create ContactID primary key field.

--- dbContacts/Tables/dbo.tblContactDetails.sql
+++ dbContacts/Tables/dbo.tblContactDetails.sql
@@ -1,8 +1,11 @@
 CREATE TABLE [dbo].[tblContactDetails]
 (
+[ContactID] [int] NOT NULL IDENTITY(1, 1),
 [FirstName] [varchar] (100) COLLATE Latin1_General_CI_AS NULL,
 [Surname] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
 [Telephone] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
 [Address] [varchar] (250) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[tblContactDetails] ADD 
+CONSTRAINT [PK_tblContactDetails] PRIMARY KEY CLUSTERED  ([ContactID]) ON [PRIMARY]
 GO
