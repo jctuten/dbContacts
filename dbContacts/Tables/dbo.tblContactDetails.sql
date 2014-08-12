5739ba0d3e511b7d00ddf9b8721c434880d58b00
James Tuten <james.tuten@red-gate.com>
2014-08-12 10:58:57 +0100

    Split Name column into Forename and Surname

---
+++ dbContacts/Tables/dbo.tblContactDetails.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[tblContactDetails]
+(
+[FirstName] [varchar] (100) COLLATE Latin1_General_CI_AS NULL,
+[Surname] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
+[Telephone] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
+[Address] [varchar] (250) COLLATE Latin1_General_CI_AS NULL
+) ON [PRIMARY]
+GO
